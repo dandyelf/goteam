6f68867bc09841a357c648601801708ce319cd8f
leftrana <dandyelf32@gmail.com>
2024-05-12 17:02:14 +0300

    lil fix

--- src/main.go
+++ src/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	err := Hallo()
+	if err != nil {
+		fmt.Println("Hallo, ERROR")
+	}
+}
+
+func Hallo() error {
+	var err error
 	fmt.Println("Hallo world?")
+	return err
 }
